09e912818c2d834c2de117e1e68396580edc3b00
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2021-04-12 10:12:35 -0500

    update connection db

--- tuinmueblecolombia/config/clases/conexion.php
+++ tuinmueblecolombia/config/clases/conexion.php
@@ -1,13 +1,13 @@
 <?php 
 class conexion{
-	private $servidor="localhost";
-	private $usuario="root";
+	private $servidor="198.52.110.228";
+	private $usuario="daniel";
 	private $bd="tu_inmueble_co";
-	private $password="";
+	private $password="rkj71MfM";
 
 	public function conexion(){
 		try {
-			$conexion = new PDO('mysql:host=localhost;dbname='.$this->bd,$this->usuario,$this->password);
+			$conexion = new PDO('mysql:host=198.52.110.228;dbname='.$this->bd,$this->usuario,$this->password);
 			$conexion->exec("SET NAMES utf8");
 			return $conexion;
 		} catch (PDOException $e) {
