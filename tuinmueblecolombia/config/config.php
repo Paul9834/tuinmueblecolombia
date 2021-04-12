464ff9bebabc31706365ce073e5032f4356273a0
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2021-04-12 10:50:23 -0500

    update route

--- tuinmueblecolombia/config/config.php
+++ tuinmueblecolombia/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 // RUTA PRINCIPAL
-define('RUTA', 'http://localhost/tuinmueblecolombia/');
+define('RUTA', 'https://lainmobiliariaco.herokuapp.com//tuinmueblecolombia/');
 
 define('ENTIDAD', 'Tu Inmueble');
 define('TITULO', 'Tu Inmueble');
